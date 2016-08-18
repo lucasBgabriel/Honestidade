07f4c65a0cd034c5e01e9adae9e68704c1fa4531
Lucas Batista Gabriel <lucas.bgabriel@gmail.com>
2016-08-18 20:56:52 -0300

    show

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/ChatApplication.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/ChatApplication.java
@@ -1,18 +1,18 @@
 package com.github.nkzawa.socketio.androidchat;
 
 import android.app.Application;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
-
-import java.net.URISyntaxException;
 
 public class ChatApplication extends Application {
 
     private Socket mSocket;
     {
         try {
+
             mSocket = IO.socket(Constants.CHAT_SERVER_URL);
-        } catch (URISyntaxException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
